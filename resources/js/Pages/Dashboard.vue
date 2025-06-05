28608f4ce1ff76eee726ce00fc5749514502b20a
phedwin <phedwinsching@students.uonbi.ac.ke>
2025-06-05 19:27:49 +0300

    - complete auth, better design(well open to opinion)  - reset email  - step forward, serve local content
    Signed-off-by: phedwin <phedwinsching@students.uonbi.ac.ke>

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,27 +1,23 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="Calestous Juma Legacy Foundation" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
-                Dashboard
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                CJLF HUB 🐟
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        You're logged in!
+                        TODO ** HERE --- * A WAY TO SERVE LOCAL CONTENT
                     </div>
                 </div>
             </div>
